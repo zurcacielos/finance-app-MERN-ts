fc6d44539eead95287e9a4c3868ebe15eba41ba7
zurcacielos <fabianse@gmail.com>
2023-03-27 21:05:16 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Finance Dashboard App
+
+A MERN Finance Dashboard App
+
+
+# TODO
+- check issues
+- .env files are intentionally missing and excluded from .gitignore
+- they should be managed with AWS secrets manager
---
+++ server/index.js
@@ -0,0 +1,52 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import kpiRoutes from "./routes/kpi.js";
+import productRoutes from "./routes/product.js";
+import transactionRoutes from "./routes/transaction.js";
+import KPI from "./models/KPI.js";
+import Product from "./models/Product.js";
+import Transaction from "./models/Transaction.js";
+import { kpis, products, transactions } from "./data/data.js";
+
+/* CONFIGURATIONS */
+dotenv.config();
+const app = express();
+app.use(express.json());
+// adding Helmet to enhance Rest API's security
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+// adding morgan to log HTTP requests
+app.use(morgan("common"));
+// using bodyParser to parse JSON bodies into JS objects
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// enabling CORS for all requests
+app.use(cors());
+
+/* ROUTES */
+app.use("/kpi", kpiRoutes);
+app.use("/product", productRoutes);
+app.use("/transaction", transactionRoutes);
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 9000;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(async () => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    /* ADD DATA ONE TIME ONLY OR AS NEEDED */
+    // await mongoose.connection.db.dropDatabase();
+    // KPI.insertMany(kpis);
+    // Product.insertMany(products);
+    // Transaction.insertMany(transactions);
+  })
+  .catch((error) => console.log(`${error} did not connect`));
